ed188f6bc048959e9fe294f4db517d613b57f690
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-10-25 18:16:45 -0400

    Add tests checking flatness in most of the DefaultSpaces

--- src/GR/SpaceTests.java
+++ src/GR/SpaceTests.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static GR.DefaultSpaces.*;
 import static GR.LinearAlgebraTools.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static tools.DefaultFunctions.*;
 
 public class SpaceTests {
@@ -28,6 +27,83 @@ public class SpaceTests {
         assertTrue(isZero(test1));
         assertTrue(isZero(test2));
         assertTrue(isZero(test3));
+    }
+
+    @Test
+    void isPolarFlat() {
+        GeneralFunction[][][][] test1 = polar.riemannTensor();
+        GeneralFunction[][] test2 = polar.ricciTensor();
+        GeneralFunction test3 = polar.ricciScalar();
+
+        assertTrue(isZero(test1));
+        assertTrue(isZero(test2));
+        assertTrue(isZero(test3));
+    }
+
+    @Test
+    void isSphericalFlat() {
+        GeneralFunction[][][][] test1 = spherical.riemannTensor();
+        GeneralFunction[][] test2 = spherical.ricciTensor();
+        GeneralFunction test3 = spherical.ricciScalar();
+
+        assertTrue(isZero(test1));
+        assertTrue(isZero(test2));
+        assertTrue(isZero(test3));
+    }
+
+    @Test
+    void isCylindricalFlat() {
+        GeneralFunction[][][][] test1 = cylindrical.riemannTensor();
+        GeneralFunction[][] test2 = cylindrical.ricciTensor();
+        GeneralFunction test3 = cylindrical.ricciScalar();
+
+        assertTrue(isZero(test1));
+        assertTrue(isZero(test2));
+        assertTrue(isZero(test3));
+    }
+
+    @Test
+    void isMinkowskiFlat() {
+        GeneralFunction[][][][] test1 = minkowski.riemannTensor();
+        GeneralFunction[][] test2 = minkowski.ricciTensor();
+        GeneralFunction test3 = minkowski.ricciScalar();
+
+        assertTrue(isZero(test1));
+        assertTrue(isZero(test2));
+        assertTrue(isZero(test3));
+    }
+
+    @Test
+    void isS2NotFlat() {
+        GeneralFunction[][][][] test1 = s2.riemannTensor();
+        GeneralFunction[][] test2 = s2.ricciTensor();
+        GeneralFunction test3 = s2.ricciScalar();
+
+        assertFalse(isZero(test1));
+        assertFalse(isZero(test2));
+        assertFalse(isZero(test3));
+    }
+
+    @Test
+    void isS3NotFlat() {
+        GeneralFunction[][][][] test1 = s3.riemannTensor();
+        GeneralFunction[][] test2 = s3.ricciTensor();
+        GeneralFunction test3 = s3.ricciScalar();
+
+        assertFalse(isZero(test1));
+        assertFalse(isZero(test2));
+        assertFalse(isZero(test3));
+    }
+
+    @Test
+    void isSchwarzschildInSchwarzschildCoordinatesNotFlat() {
+        GeneralFunction[][][][] test1 = schwarzschildInSchwarzschildCoordinates.riemannTensor();
+        GeneralFunction[][] test2 = schwarzschildInSchwarzschildCoordinates.ricciTensor();
+        GeneralFunction test3 = schwarzschildInSchwarzschildCoordinates.ricciScalar();
+
+        assertFalse(isZero(test1));
+        assertFalse(isZero(test2));
+        assertFalse(isZero(test3));
     }
 
 }
