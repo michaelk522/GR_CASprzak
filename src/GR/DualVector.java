5676adf63c17b27f4e5cbee1510efa10f3d9fdc4
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-10-25 17:27:46 -0400

    Delete Tensor objects

--- src/GR/DualVector.java
+++
@@ -1,7 +0,0 @@
-package GR;
-
-public class DualVector extends Tensor{
-    public DualVector(Space space, int tangentSpaces, int cotangentSpaces) {
-        super(space, tangentSpaces, cotangentSpaces);
-    }
-}
--- src/GR/Vector.java
+++
@@ -1,7 +0,0 @@
-package GR;
-
-public class Vector extends Tensor{
-    public Vector(Space space, int tangentSpaces, int cotangentSpaces) {
-        super(space, tangentSpaces, cotangentSpaces);
-    }
-}
