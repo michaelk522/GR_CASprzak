355ffb34123444e6d5602dc7d11d921b45deae10
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-10-25 16:48:18 -0400

    Add isDiagonal to Metric and InverseMetric

--- src/GR/InverseMetric.java
+++ src/GR/InverseMetric.java
@@ -4,10 +4,18 @@ import functions.GeneralFunction;
 
 public class InverseMetric extends Tensor {
     public GeneralFunction[][] matrix;
+    public boolean isDiagonal;
 
 
     public InverseMetric(Space space, GeneralFunction[][] inverseMetric) {
         super(space, 2, 0);
         this.matrix = inverseMetric;
+        isDiagonal = false;
+    }
+
+    public InverseMetric(Space space, GeneralFunction[][] metric, boolean isDiagonal) {
+        super(space, 0, 2);
+        this.matrix = metric;
+        this.isDiagonal = isDiagonal;
     }
 }
--- src/GR/Metric.java
+++ src/GR/Metric.java
@@ -4,9 +4,17 @@ import functions.GeneralFunction;
 
 public class Metric extends Tensor {
     public GeneralFunction[][] matrix;
+    public boolean isDiagonal;
 
     public Metric(Space space, GeneralFunction[][] metric) {
         super(space, 0, 2);
         this.matrix = metric;
+        isDiagonal = false;
+    }
+
+    public Metric(Space space, GeneralFunction[][] metric, boolean isDiagonal) {
+        super(space, 0, 2);
+        this.matrix = metric;
+        this.isDiagonal = isDiagonal;
     }
 }
