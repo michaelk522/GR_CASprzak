06bbd8ccc0df946b754a13b222c8a53b5f2954d0
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-10-25 00:03:34 -0400

    Test subMatrix

--- src/GR/GRTest.java
+++ src/GR/GRTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import parsing.FunctionParser;
 import tools.singlevariable.Extrema;
 
+import java.util.Arrays;
+
 import static GR.LinearAlgebraTools.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static tools.DefaultFunctions.*;
@@ -29,6 +31,44 @@ public class GRTest {
         };
 
         assertFalse(isSquare(test));
+    }
+
+
+    @Test
+    void simpleSubMatrix() {
+        GeneralFunction[][] test1 = new GeneralFunction[][]{
+                new GeneralFunction[]{ONE, TWO},
+                new GeneralFunction[]{HALF, ONE},
+        };
+
+        GeneralFunction[][] test2 = new GeneralFunction[][]{
+                new GeneralFunction[]{HALF}
+        };
+
+        assertArrayEquals(test2, subMatrix(test1, 0, 1));
+    }
+
+
+    @Test
+    void notSoSimpleSubMatrix() {
+        GeneralFunction[][] test1 = new GeneralFunction[][]{
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE, ONE},
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE, ONE},
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE, ONE},
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE, ONE},
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE, ONE},
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE, ONE}
+        };
+
+        GeneralFunction[][] test2 = new GeneralFunction[][]{
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE},
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE},
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE},
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE},
+                new GeneralFunction[]{ONE, ONE, ONE, ONE, ONE},
+        };
+
+        assertArrayEquals(test2, subMatrix(test1, 3, 2));
     }
 
 
