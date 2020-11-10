ec7d00b52863d0a54d844c6dc6d185fecc967b3c
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-11-10 12:07:34 -0500

    Finish documenting Metric and InverseMetric

--- src/GR/src/InverseMetric.java
+++ src/GR/src/InverseMetric.java
@@ -3,18 +3,35 @@ package GR.src;
 import GR.GR;
 import functions.GeneralFunction;
 
+/**
+ * This class contains the the matrix of an {@code InverseMetric} and whether or not it is diagonal.
+ */
 public class InverseMetric{
+    /**
+     * the matrix representation of the {@code InverseMetric}
+     */
     public GeneralFunction[][] matrix;
+    /**
+     * Denotes whether or not the matrix is diagonal
+     */
     public boolean isDiagonal;
 
-
+    /**
+     * Defines a new {@link InverseMetric}
+     * @param inverseMetric the matrix of the {@code InverseMetric}
+     */
     public InverseMetric(GeneralFunction[][] inverseMetric) {
         this.matrix = inverseMetric;
         isDiagonal = false;
     }
 
-    public InverseMetric(GeneralFunction[][] metric, boolean isDiagonal) {
-        this.matrix = metric;
+    /**
+     * Defines a new {@link InverseMetric}
+     * @param inverseMetric the matrix of the {@code InverseMetric}
+     * @param isDiagonal denotes whether or not the matrix is diagonal
+     */
+    public InverseMetric(GeneralFunction[][] inverseMetric, boolean isDiagonal) {
+        this.matrix = inverseMetric;
         this.isDiagonal = isDiagonal;
     }
 }
--- src/GR/src/Metric.java
+++ src/GR/src/Metric.java
@@ -3,15 +3,33 @@ package GR.src;
 import GR.GR;
 import functions.GeneralFunction;
 
+/**
+ * This class contains the the matrix of an {@code Metric} and whether or not it is diagonal.
+ */
 public class Metric{
+    /**
+     * the matrix representation of the {@code Metric}
+     */
     public GeneralFunction[][] matrix;
+    /**
+     * Denotes whether or not the matrix is diagonal
+     */
     public boolean isDiagonal;
 
+    /**
+     * Defines a new {@link Metric}
+     * @param metric the matrix of the {@code Metric}
+     */
     public Metric(GeneralFunction[][] metric) {
         this.matrix = metric;
         isDiagonal = false;
     }
 
+    /**
+     * Defines a new {@link Metric}
+     * @param metric the matrix of the {@code Metric}
+     * @param isDiagonal denotes whether or not the matrix is diagonal
+     */
     public Metric(GeneralFunction[][] metric, boolean isDiagonal) {
         this.matrix = metric;
         this.isDiagonal = isDiagonal;
